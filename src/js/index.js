fdad1b567f80b3038d044a3157d2332170bce50b
fajrwajr <bookreaderfajr@gmail.com>
2021-10-01 05:46:43 +0000

    Changed Position

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import "../style/index.scss";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+  
     {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
@@ -27,16 +27,20 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let position = `<ul class="position-right">${variables.socialMediaPosition}</ul>`;
 
+  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.socialMediaPosition == "position-left")
+    position = `<ul>${variables.socialMediaPosition}</ul>`;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
+            ${position}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
@@ -64,7 +68,7 @@ window.onload = function() {
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
-    name: null,
+    name: "Your Name",
     lastname: null,
     role: null,
     country: null,
