350269e5b08305487172eb0ef9993c78d3ebebc9
fajrwajr <bookreaderfajr@gmail.com>
2021-10-02 03:59:03 +0000

    Change Extension

--- src/js/index.js
+++ src/js/index.js
@@ -45,13 +45,11 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h4>${variables.city}, ${variables.country}</h4>
-          <h3>${variables.twitter}, ${variables.linkedin}</h3>
-          <h3>${variables.instagram}, ${variables.github}</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
